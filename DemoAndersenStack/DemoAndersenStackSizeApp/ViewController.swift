1cf076ae4182af5787d2f879a12a606039c8b52c
Viacheslav <savitsky,fit@gmail.com>
2019-10-28 12:44:18 +0200

    first commit

---
+++ DemoAndersenStack/DemoAndersenStackSizeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoAndersenStackSizeApp
+//
+//  Created by admin on 28.10.2019.
+//  Copyright © 2019 Viacheslav Savitsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
