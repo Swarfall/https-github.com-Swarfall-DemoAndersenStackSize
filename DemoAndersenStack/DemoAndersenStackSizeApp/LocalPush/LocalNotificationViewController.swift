0d958f7b764a0927e685fde7aa04ad8a9b5a4ee7
Viacheslav <savitsky,fit@gmail.com>
2019-10-30 14:57:59 +0200

    add button to notification

--- DemoAndersenStack/DemoAndersenStackSizeApp/LocalPush/LocalNotificationViewController.swift
+++ DemoAndersenStack/DemoAndersenStackSizeApp/LocalPush/LocalNotificationViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import UserNotifications
 
-class LocalNotificationViewController: UIViewController {
+class LocalNotificationViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     let center = UNUserNotificationCenter.current()
     let content = UNMutableNotificationContent()
@@ -21,34 +21,75 @@ class LocalNotificationViewController: UIViewController {
     }
     
     @IBAction func didTapLocalNotification(_ sender: Any) {
-        localNotification()
+        //localNotification()
+        
+        scheduleNotification(notificationType: "jopa")
     }
-    
-    private func localNotification() {
-        content.title = "It's title"
-        content.subtitle = "It's subtitle"
-        content.body = "It's body"
+    func scheduleNotification(notificationType: String) {
+        
+        let userActions = "User Actions"
+        
+        content.title = notificationType
+        content.body = "This is example how to create " + notificationType
         content.sound = UNNotificationSound.default
-        content.badge = 47
+        content.badge = 1
+        content.categoryIdentifier = userActions
         
-        let date = Date(timeIntervalSinceNow: 5)
-        
-        let dateConponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute , .second], from: date)
-        
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateConponents, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: "content", content: content, trigger: trigger)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let identifier = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
         
         center.add(request) { (error) in
-            if error != nil {
-                print(error)
+            if let error = error {
+                print("Error \(error.localizedDescription)")
             }
         }
+        
         let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
         let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
-        let category = UNNotificationCategory(identifier: "content", actions: [snoozeAction, deleteAction], intentIdentifiers: [], options: [])
+        let category = UNNotificationCategory(identifier: userActions, actions: [snoozeAction, deleteAction], intentIdentifiers: [], options: [])
         
         center.setNotificationCategories([category])
-        
     }
+    //  private func localNotification() {
+//        content.title = "It's title"
+//        content.subtitle = "It's subtitle"
+//        content.body = "It's body"
+//        content.sound = UNNotificationSound.default
+//        content.badge = 47
+//
+//        let repeatAction = UNNotificationAction(identifier:"repeat",
+//            title:"Repeat",options:[])
+//        let changeAction = UNTextInputNotificationAction(identifier:
+//            "change", title: "Change Message", options: [])
+//
+//        let category = UNNotificationCategory(identifier: "actionCategory",
+//             actions: [repeatAction, changeAction],
+//            intentIdentifiers: [], options: [])
+//
+//        content.categoryIdentifier = "actionCategory"
+//
+//        UNUserNotificationCenter.current().setNotificationCategories([category])
+//
+//        let date = Date(timeIntervalSinceNow: 5)
+//
+//        let dateConponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute , .second], from: date)
+//
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateConponents, repeats: false)
+//
+//        let request = UNNotificationRequest(identifier: "content", content: content, trigger: trigger)
+//
+//        center.add(request) { (error) in
+//            if error != nil {
+//                print(error)
+//            }
+//        }
+//        center.delegate = self
+//        let snoozeAction = UNNotificationAction(identifier: "Snooze", title: "Snooze", options: [])
+//        let deleteAction = UNNotificationAction(identifier: "Delete", title: "Delete", options: [.destructive])
+//        let notificationCategory = UNNotificationCategory(identifier: "content", actions: [snoozeAction, deleteAction], intentIdentifiers: [], options: [])
+//
+//        UNUserNotificationCenter.current().setNotificationCategories([notificationCategory])
+//
+//    }
 }
